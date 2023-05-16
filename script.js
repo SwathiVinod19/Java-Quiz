8d9d8c595d40ed17def7b98cc4ee804d25630187
SwathiVinod19 <swathi.vinod@gmail.com>
2023-05-15 22:58:18 -0400

    added js part two

--- script.js
+++ script.js
@@ -79,3 +79,208 @@ function reset(){
     answerD.style.color = "rgb(68, 68, 68)";  
     
 }
+//This function has a welcome message and also displays the first quiz question
+//The timer is also set within this function
+
+function greeting() {
+    welcomeEle.innerHTML = ""
+    headerText.innerHTML = ""
+    headerText.innerHTML = "Hello! " + userNam.value +", welcome to the javascript quiz"
+    greetingInput.style.display = "none"
+    rulesOne.style.display = "none"
+    rulesTwo.style.display = "none"
+    saveButton.style.display = "none"
+    quiz.style.display = "block";
+    qn1Button.style.display = "block";
+    question.innerHTML= questionList[indexNumber];
+    answerA.innerHTML = aAnswerList[indexNumber];
+    answerB.innerHTML = bAnswerList[indexNumber];
+    answerC.innerHTML = cAnswerList[indexNumber];
+    answerD.innerHTML = dAnswerList[indexNumber];
+
+    var timerInterval = setInterval(
+
+    function updateCountdown(){
+        let minute = Math.floor(time / 60);
+        let seconds = time % 60;
+        time--;
+        coundownEl.innerHTML = minute+" : "+seconds;
+    
+    if(minute == 0 && seconds == 0){
+    clearInterval(timerInterval)
+    quizOver();
+    }
+    if(minute < 0 && seconds < 0){
+        clearInterval(timerInterval)
+        coundownEl.innerHTML = "0:0";
+        quizOver();
+        }
+    },  1000);
+
+}
+
+//linking the options
+
+function functionA(){
+    answerA.style.color = "orangered";
+    answerB.style.color = "rgb(68, 68, 68)";
+    answerC.style.color = "rgb(68, 68, 68)";
+    answerD.style.color = "rgb(68, 68, 68)";
+    qnChoice = "a";
+
+}
+function functionB(){
+    answerB.style.color = "orangered";
+    answerA.style.color = "rgb(68, 68, 68)";
+    answerC.style.color = "rgb(68, 68, 68)";
+    answerD.style.color = "rgb(68, 68, 68)";
+    qnChoice = "b";
+}
+function functionC(){
+    answerC.style.color = "orangered";
+    answerB.style.color = "rgb(68, 68, 68)";
+    answerA.style.color = "rgb(68, 68, 68)";
+    answerD.style.color = "rgb(68, 68, 68)";
+    qnChoice = "c";
+}
+function functionD(){
+    answerD.style.color = "orangered";
+    answerB.style.color = "rgb(68, 68, 68)";
+    answerC.style.color = "rgb(68, 68, 68)";
+    answerA.style.color = "rgb(68, 68, 68)";
+    qnChoice = "d";
+}
+
+//the next questions are displayed in these following functions
+//checks for the correct answer
+//increments the score when the correct answer is clicked on by the user
+//decreases time with the wrong answer
+
+function submitQ1() {
+    if (qnChoice == "a"){
+        score += 1;    
+    }
+    else{
+    time -= 15;
+    }
+    indexNumber += 1;
+    reset();
+    qn1Button.style.display = "none";
+    qn2Button.style.display = "block";    
+}
+
+function submitQ2() {
+    if (qnChoice == "d"){
+        score += 1;      
+    }
+    else{
+        time -= 15;
+        }
+    
+    indexNumber += 1;
+    reset();
+    qn1Button.style.display = "none";
+    qn2Button.style.display = "none";
+    qn3Button.style.display = "block";
+}
+
+function submitQ3() {
+    if (qnChoice == "c"){
+        score += 1; 
+        
+    }
+    else{
+        time -= 15;
+        }
+    
+    indexNumber += 1;
+    reset();
+    qn1Button.style.display = "none";
+    qn2Button.style.display = "none";
+    qn3Button.style.display = "none";
+    qn4Button.style.display = "block"
+}
+
+function submitQ4() {
+    if (qnChoice == "a"){
+        score += 1;     
+    }
+    else{
+        time -= 15;
+        }
+    
+    indexNumber += 1;
+    reset();
+    qn1Button.style.display = "none";
+    qn2Button.style.display = "none";
+    qn3Button.style.display = "none";
+    qn4Button.style.display = "none";
+    qn5Button.style.display = "block";
+}
+
+
+function submitQ5() {
+    if (qnChoice == "d" && time > 0){
+        score += 1; 
+        
+    }
+    else if(time == 0){
+        quizOver();
+    }
+    else{
+       quizOver();
+        }
+    quizOver();
+    }
+
+
+function quizOver(){
+    coundownEl.innerHTML = "0:0"; 
+    time = 0;  
+    headerText.innerHTML = " Your score is :" +score;
+    quiz.style.display = "none";
+    qn1Button.style.display = "none";
+    qn2Button.style.display = "none";  
+    qn3Button.style.display = "none";
+    saveButton.style.display = "block";
+    card.style.display = "block";
+   
+
+}
+
+//To store the username and score in local storage
+
+
+function saveLastScore() {
+    
+    var playerScore = {
+
+        userNam: userNam.value,
+        score: score.valueOf()
+
+    };
+    localStorage.setItem("playerScore", JSON.stringify(playerScore));
+}
+function renderLastScore() {
+   
+    var lastScore = JSON.parse(localStorage.getItem("playerScore"));
+    
+    if (lastScore !== null) {
+    document.getElementById("saved-name").innerHTML = lastScore.userNam;
+    document.getElementById("saved-score").innerHTML = lastScore.score;
+    
+    } else {
+      return;
+    }
+  }
+  saveButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    saveLastScore();
+    renderLastScore();
+    });
+
+    function init() {
+        
+        renderLastScore();
+      }
+      init();
